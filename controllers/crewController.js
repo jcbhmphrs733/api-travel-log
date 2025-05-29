9aba644507be99d24a5a2fc54236ff7d03200155
Jacob Humphreys <jacob_humphreys@icloud.com>
2025-05-28 18:56:47 -0600

    updated all functions to use a try/catch method instead of abstract function.

--- controllers/crewController.js
+++ controllers/crewController.js
@@ -5,88 +5,97 @@ const ObjectId = require("mongodb").ObjectId;
 const getCrew = async (req, res) => {
   //#swagger.tags = ['Crew']
   //#swagger.description = 'Get all crew members'
-  const response = await mongodb.getDb().collection("crew").find();
-  response
-    .toArray()
-    .then((log) => {
+  try {
+    const response = await mongodb.getDb().collection("crew").find();
+    response.toArray().then((log) => {
       res.setHeader("Content-Type", "application/json");
       res.status(200).json(log);
-    })
-    .catch((err) => {
-      console.error("Error fetching crew:", err);
     });
+  } catch (err) {
+    console.error("Error fetching crew:", err);
+    res
+      .status(500)
+      .json({ message: "An error occurred while fetching crew members." });
+  }
 };
 
 const getCrewMember = async (req, res) => {
   //#swagger.tags = ['Crew']
   //#swagger.description = 'Get a specific crew member by ID'
-  const entryId = ObjectId.createFromHexString(req.params.id);
+  try {
+    const entryId = ObjectId.createFromHexString(req.params.id);
 
-  const response = await mongodb
-    .getDb()
-    .collection("crew")
-    .findOne({ _id: entryId });
+    const response = await mongodb
+      .getDb()
+      .collection("crew")
+      .findOne({ _id: entryId });
 
-  if (!response) {
-    throw createError(404, "Crew member ID does not exist.");
+    if (!response) {
+      throw createError(404, "Crew member ID does not exist.");
+    }
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(error.status || 500).json({
+      message:
+        error.message || "An error occurred while fetching the crew member.",
+    });
   }
-  res.setHeader("Content-Type", "application/json");
-  res.status(200).json(response);
 };
 
 const postCrewMember = async (req, res) => {
   //#swagger.tags = ['Crew']
   //#swagger.description = 'Create a new crew member'
-  const entry = {
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    rank: req.body.rank,
-    age: req.body.age,
-    home_country: req.body.home_country,
-    time_onboard_months: req.body.time_onboard_months,
-    hire_date: req.body.hire_date
-  };
+  try {
+    const entry = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      rank: req.body.rank,
+      age: req.body.age,
+      home_country: req.body.home_country,
+      time_onboard_months: req.body.time_onboard_months,
+      hire_date: req.body.hire_date,
+    };
 
-  const response = await mongodb
-    .getDb()
-    .collection("crew")
-    .insertOne(entry);
-  if (response.acknowledged) {
+    const response = await mongodb.getDb().collection("crew").insertOne(entry);
+    if (response.acknowledged) {
+      res.status(201).json({
+        message: "Entry created successfully",
+        id: response.insertedId,
+      });
+    }
+  } catch (error) {
+    console.error("Error creating crew member:", error);
     res
-      .status(201)
-      .json({ message: "Entry created successfully", id: response.insertedId });
-  } else {
-    throw createError(500, "An error occurred while creating the crew member.");
+      .status(500)
+      .json({ message: "An error occurred while creating the crew member." });
   }
 };
 
 const updateCrewMember = async (req, res) => {
   //#swagger.tags = ['Crew']
   //#swagger.description = 'Update an existing crew member by ID'
-  const crewId = ObjectId.createFromHexString(req.params.id);
   try {
-    if (!crewId) {
-      throw createError(404, "Entry does not exist.");
+    const crewId = ObjectId.createFromHexString(req.params.id);
+
+    const entry = {
+      fist_name: req.body.fist_name,
+      last_name: req.body.last_name,
+      rank: req.body.rank,
+      age: req.body.age,
+      home_country: req.body.home_country,
+      time_onboard_months: req.body.time_onboard_months,
+      hire_date: req.body.hire_date,
+    };
+    const response = await mongodb
+      .getDb()
+      .collection("crew")
+      .replaceOne({ _id: crewId }, entry);
+    if (response.modifiedCount > 0) {
+      res.status(200).send();
     }
   } catch (error) {
-    console.log(error.message);
-  }
-  const entry = {
-    fist_name: req.body.fist_name,
-    last_name: req.body.last_name,
-    rank: req.body.rank,
-    age: req.body.age,
-    home_country: req.body.home_country,
-    time_onboard_months: req.body.time_onboard_months,
-    hire_date: req.body.hire_date
-  };
-  const response = await mongodb
-    .getDb()
-    .collection("crew")
-    .replaceOne({ _id: crewId }, entry);
-  if (response.modifiedCount > 0) {
-    res.status(200).send();
-  } else {
+    console.error("Error updating crew member:", error);
     throw createError(500, "An error occurred while updating the crew member.");
   }
 };
@@ -94,7 +103,7 @@ const updateCrewMember = async (req, res) => {
 const deleteCrewMember = async (req, res) => {
   //#swagger.tags = ['Crew']
   //#swagger.description = 'Delete a crew member by ID'
-  try{
+  try {
     const crewId = ObjectId.createFromHexString(req.params.id);
     const response = await mongodb
       .getDb()
@@ -103,36 +112,41 @@ const deleteCrewMember = async (req, res) => {
     if (response.deletedCount === 0) {
       throw createError(404, "Crew member ID does not exist.");
     } else {
-      res
-        .status(200)
-        .json({message: "Crew member deleted succesfully."});
+      res.status(200).json({ message: "Crew member deleted succesfully." });
     }
   } catch (error) {
     console.error("Error deleting crew member:", error);
-    res.status(500).json({ message: "An error occurred while deleting the crew member." });
+    res
+      .status(500)
+      .json({ message: "An error occurred while deleting the crew member." });
   }
 };
 
 const postMany = async (req, res) => {
   //#swagger.tags = ['Crew']
   //#swagger.description = 'Create multiple crew members at once'
-  const entries = req.body; // Assuming entries is an array of entry objects
-  if (!Array.isArray(entries)) {
-    return res
-      .status(400)
-      .json({ message: "Invalid input format. Expected an array of entries." });
-  }
+  try {
+    const entries = req.body; // Assuming entries is an array of entry objects
+    if (!Array.isArray(entries)) {
+      return res
+        .status(400)
+        .json({
+          message: "Invalid input format. Expected an array of entries.",
+        });
+    }
 
-  const response = await mongodb
-    .getDb()
-    .collection("crew")
-    .insertMany(entries);
-  if (response.acknowledged) {
-    res.status(201).json({
-      message: "Entries created successfully",
-      ids: response.insertedIds,
-    });
-  } else {
+    const response = await mongodb
+      .getDb()
+      .collection("crew")
+      .insertMany(entries);
+    if (response.acknowledged) {
+      res.status(201).json({
+        message: "Entries created successfully",
+        ids: response.insertedIds,
+      });
+    }
+  } catch {
+    console.error("Error creating multiple crew members:", error);
     res.status(500).json({ message: "Failed to create entries" });
   }
 };
--- controllers/logController.js
+++ controllers/logController.js
@@ -21,80 +21,91 @@ const getLog = async (req, res) => {
 const getEntry = async (req, res) => {
   //#swagger.tags = ['Log']
   //#swagger.description = 'Get a specific log entry by ID'
-  const entryId = ObjectId.createFromHexString(req.params.id);
-
-  const response = await mongodb
-    .getDb()
-    .collection("log")
-    .findOne({ _id: entryId });
-
-  if (!response) {
-    throw createError(404, "Entry does not exist.");
-  }
-  res.setHeader("Content-Type", "application/json");
-  res.status(200).json(response);
-};
-
-const postEntry = async (req, res) => {
-  //#swagger.tags = ['Log']
-  //#swagger.description = 'Create a new log entry'
-  const entry = {
-    date: req.body.date,
-    time: req.body.time,
-    latitude: req.body.latitude,
-    longitude: req.body.longitude,
-    temperature: req.body.temperature,
-    wind_speed: req.body.wind_speed,
-    wind_direction: req.body.wind_direction,
-    heading: req.body.heading,
-  };
-
-  const response = await mongodb.getDb().collection("log").insertOne(entry);
-  if (response.acknowledged) {
-    res
-      .status(201)
-      .json({ message: "Entry created successfully", id: response.insertedId });
-  } else {
-    throw createError(500, "An error occurred while creating the log entry.");
-  }
-};
-
-const updateEntry = async (req, res) => {
-  //#swagger.tags = ['Log']
-  //#swagger.description = 'Update an existing log entry by ID'
-  const entryId = ObjectId.createFromHexString(req.params.id);
   try {
-    if (!entryId) {
+    const entryId = ObjectId.createFromHexString(req.params.id);
+
+    const response = await mongodb
+      .getDb()
+      .collection("log")
+      .findOne({ _id: entryId });
+
+    if (!response) {
       throw createError(404, "Entry does not exist.");
     }
+    res.setHeader("Content-Type", "application/json"); 
+    res.status(200).json(response);
   } catch (error) {
-    console.log(error.message);
+    res.status(error.status || 500).json({
+      message:
+        error.message || "An error occurred while fetching the log entry.",
+    });
   }
-  const entry = {
-    date: req.body.date,
-    time: req.body.time,
-    latitude: req.body.latitude,
-    longitude: req.body.longitude,
-    temperature: req.body.temperature,
-    wind_speed: req.body.wind_speed,
-    wind_direction: req.body.wind_direction,
-    heading: req.body.heading,
+}
+
+  const postEntry = async (req, res) => {
+    //#swagger.tags = ['Log']
+    //#swagger.description = 'Create a new log entry'
+    try {
+      const entry = {
+        date: req.body.date,
+        time: req.body.time,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+        temperature: req.body.temperature,
+        wind_speed: req.body.wind_speed,
+        wind_direction: req.body.wind_direction,
+        heading: req.body.heading,
+      };
+
+      const response = await mongodb.getDb().collection("log").insertOne(entry);
+      if (response.acknowledged) {
+        res.status(201).json({
+          message: "Entry created successfully",
+          id: response.insertedId,
+        });
+      }
+    } catch (error) {
+      console.error("Error creating log entry:", error);
+      throw createError(500, "An error occurred while creating the log entry.");
+    }
   };
-  const response = await mongodb
-    .getDb()
-    .collection("log")
-    .replaceOne({ _id: entryId }, entry);
-  if (response.modifiedCount > 0) {
-    res.status(200).send();
-  } else {
-    throw createError(500, "An error occurred while updating the log entry.");
-  }
+
+  const updateEntry = async (req, res) => {
+    //#swagger.tags = ['Log']
+    //#swagger.description = 'Update an existing log entry by ID'
+    try {
+      const entryId = ObjectId.createFromHexString(req.params.id);
+      if (!entryId) {
+        throw createError(404, "Entry does not exist.");
+      }
+
+      const entry = {
+        date: req.body.date,
+        time: req.body.time,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+        temperature: req.body.temperature,
+        wind_speed: req.body.wind_speed,
+        wind_direction: req.body.wind_direction,
+        heading: req.body.heading,
+      };
+      const response = await mongodb
+        .getDb()
+        .collection("log")
+        .replaceOne({ _id: entryId }, entry);
+      if (response.modifiedCount > 0) {
+        res.status(200).send();
+      }
+    } catch (error) {
+      console.error("Error updating log entry:", error);
+      throw createError(500, "An error occurred while updating the log entry.");
+    }
 };
 
 const deleteEntry = async (req, res) => {
   //#swagger.tags = ['Log']
   //#swagger.description = 'Delete an entry by ID'
-  try{
+  try {
     const entryId = ObjectId.createFromHexString(req.params.id);
     const response = await mongodb
       .getDb()
@@ -103,13 +114,13 @@ const deleteEntry = async (req, res) => {
     if (response.deletedCount === 0) {
       throw createError(404, "Entry ID does not exist.");
     } else {
-      res
-        .status(200)
-        .json({message: "Entry deleted succesfully."});
+      res.status(200).json({ message: "Entry deleted succesfully." });
     }
   } catch (error) {
     console.error("Error deleting entry:", error);
-    res.status(500).json({ message: "An error occurred while deleting the entry." });
+    res
+      .status(500)
+      .json({ message: "An error occurred while deleting the entry." });
   }
 };
 
@@ -117,19 +128,27 @@ const postMany = async (req, res) => {
   //#swagger.tags = ['Log']
   //#swagger.description = 'Create multiple log entries at once'
   const entries = req.body; // Assuming entries is an array of entry objects
-  if (!Array.isArray(entries)) {
-    return res
-      .status(400)
-      .json({ message: "Invalid input format. Expected an array of entries." });
-  }
+  try {
+    if (!Array.isArray(entries)) {
+      return res
+        .status(400)
+        .json({
+          message: "Invalid input format. Expected an array of entries.",
+        });
+    }
 
-  const response = await mongodb.getDb().collection("log").insertMany(entries);
-  if (response.acknowledged) {
-    res.status(201).json({
-      message: "Entries created successfully",
-      ids: response.insertedIds,
-    });
-  } else {
+    const response = await mongodb
+      .getDb()
+      .collection("log")
+      .insertMany(entries);
+    if (response.acknowledged) {
+      res.status(201).json({
+        message: "Entries created successfully",
+        ids: response.insertedIds,
+      });
+    }
+  } catch (error) {
+    console.error("Error creating log entries:", error);
     res.status(500).json({ message: "Failed to create entries" });
   }
 };
